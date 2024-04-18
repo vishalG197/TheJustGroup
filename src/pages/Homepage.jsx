608344a19fdaac9fd0bdeb0ab7d10928b990319e
unknown <vishalgiri197@gmail.com>
2024-04-18 11:29:24 +0530

    testimonial_done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import React, { lazy, Suspense } from 'react';
 import Loder from '../components/Loder';
 const LazySlider = lazy(() => import('../components/SliderComponent'));
 const Subscribe = lazy(() => import('../components/SubscribeSection'));
+const Testimonial = lazy(() => import('../components/Testimonial'));
 const Homepage = () => {
   return (
     <Suspense fallback={<Loder/>}>
@@ -13,6 +14,8 @@ const Homepage = () => {
       <div style={{width:'100%'}}>
         <img width={'100%'} src="https://t4.ftcdn.net/jpg/04/17/42/57/240_F_417425751_z8Rg86FJnDc8vUSC5Frj3CgD4owjLQTB.jpg" alt="" />
       </div>
+    
+<Testimonial/>
 <Subscribe />
   </Suspense>
   )
